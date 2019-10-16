2f770435628b01a3368b465bd5232ed8f49066ca
Erik Nava <erik.nava@gmail.com>
2019-10-16 15:32:47 -0500

    coded the is_winner method, modified turns method and printboard

--- TicTacToe.rb
+++ TicTacToe.rb
@@ -16,7 +16,7 @@ class Board
     @inner
   end
   def take_place(inpt, play)
-    @inner[inpt-1] = play ? "X" : "O"
+    @inner[inpt-1] = play ? "O" : "X"
   end
 end
 
@@ -41,6 +41,20 @@ class Game
     end
   end
 
+  def is_winner(curr)          # I'm using this instead of the game_verifier
+    winner = false
+    condition = []
+    @win.each do |cond|
+      f = (curr.include? cond[0]) && (curr.include? cond[1]) && (curr.include? cond[2])
+      condition.push(f)
+    end
+    if condition.any?
+      winner = true
+    end
+    puts @board.get_board.to_s + "\nYou Win" if winner
+    winner
+  end
+
   def turns
     if @p1current
       puts "Player 1:"
@@ -49,59 +63,66 @@ class Game
       puts "Player 2:"
       @p2.taken.push(get_input)
     end
-    @p1current = @p1current ? false : true
+    if @p1current
+      @p1current = false
+      return @p1.taken
+    else
+      @p1current = true
+      return @p2.taken
+    end
+
   end
 
-  def printboard
-    turns
-    turns
-    turns
-    turns
-    puts @p1.taken.to_s
-    puts @p2.taken.to_s
-    puts @board.get_board.to_s
+  def printboard                         # I'm using this section for the actual game and printing
+    while !(is_winner(turns))
+      puts @board.get_board.to_s
+      unless (@board.get_board.any? { |i| i.is_a? Numeric })
+        puts "No more space left to play"
+        break
+      end
+    end
   end
 end
 
-def taken(@p1.taken, @p2.taken)
-  if @p1.taken
-    puts "Wait for the next turn"
-  elsif
-    @p2.taken
-    puts "You can play"
-  else
-    puts "No more space to put your token"
-  end
+# def taken(@p1.taken, @p2.taken)         # it's not good practice to use repeated names
+#   if @p1.taken                          # also, these methods should go inside the Game class
+#     puts "Wait for the next turn"       # and we already have a method to discriminate turns
+#   elsif
+#     @p2.taken
+#     puts "You can play"
+#   else
+#     puts "No more space to put your token"
+#   end
 
 
-  def create_win
-    @win = {
-      '1', => ' ',
-      '2', => ' ',
-      '3', => ' ',
-      '4', => ' ',
-      '5', => ' ',
-      '6', => ' ',
-      '7', => ' ',
-      '8', => ' ',
-      '9', => ' ',
-    }
+#   def create_win                       # we already have a winning condition defined @win
+#     @win = {
+#       '1', => ' ',
+#       '2', => ' ',
+#       '3', => ' ',
+#       '4', => ' ',
+#       '5', => ' ',
+#       '6', => ' ',
+#       '7', => ' ',
+#       '8', => ' ',
+#       '9', => ' ',
+#     }
 
-  end
+#   end
 
-  def display_result
-    puts ""
-    puts " #{@win['1']} | #{@win['2']} | #{@win['3']}"
-    puts " #{@win['4']} | #{@win['5']} | #{@win['6']}"
-    puts " #{@win['7']} | #{@win['8']} | #{@win['9']}"
-    puts ""
-  end
+#   def display_result                      # We'll use this later
+#     puts ""
+#     puts " #{@win['1']} | #{@win['2']} | #{@win['3']}"
+#     puts " #{@win['4']} | #{@win['5']} | #{@win['6']}"
+#     puts " #{@win['7']} | #{@win['8']} | #{@win['9']}"
+#     puts ""
+#   end
 
-  def game_verifier
-    stop_game = false
-    if ((@win['1'] == @win['2'] && @win['1'] == @win['3']) && (@win['1'] != ' ' && @win['2'] != ' ' && @win['3'] != ' '))
-      stop_game = true
-      winning = @win['1']
+#   def game_verifier                       # I'll use this in a different way
+#     stop_game = false
+#     if ((@win['1'] == @win['2'] && @win['1'] == @win['3']) && (@win['1'] != ' ' && @win['2'] != ' ' && @win['3'] != ' '))
+#       stop_game = true
+#       winning = @win['1']
       
 
 
