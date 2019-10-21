22d8a5d5dfd703ef0ae5d8dd23df3b622b4925c6
Erik Nava <erik.nava@gmail.com>
2019-10-21 10:38:15 -0500

    replaced single quotes for double, as we're using a newspace character

--- bin/main.rb
+++ bin/main.rb
@@ -19,11 +19,11 @@ class Board
   end
 
   def draw_board
-    puts '\n ' + @inner[0].to_s + ' | ' + @inner[1].to_s + ' | ' + @inner[2].to_s + ' '
+    puts "\n " + @inner[0].to_s + ' | ' + @inner[1].to_s + ' | ' + @inner[2].to_s + ' '
     puts '-----------'
     puts ' ' + @inner[3].to_s + ' | ' + @inner[4].to_s + ' | ' + @inner[5].to_s + ' '
     puts '-----------'
-    puts ' ' + @inner[6].to_s + ' | ' + @inner[7].to_s + ' | ' + @inner[8].to_s + ' \n\n'
+    puts ' ' + @inner[6].to_s + ' | ' + @inner[7].to_s + ' | ' + @inner[8].to_s + " \n\n"
   end
 end
 
