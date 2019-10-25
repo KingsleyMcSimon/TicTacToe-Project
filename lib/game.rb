3cad98c92bcf958aee8d1def56f482f47aa17fa5
Erik Nava <erik.nava@gmail.com>
2019-10-25 13:38:45 -0500

    fixed complexity issue

--- lib/game.rb
+++ lib/game.rb
@@ -20,9 +20,13 @@ class Game
     current = @p1turn ? @play1 : @play2
     @board.take_place(sel, current)
     current.taken.push(sel)
-    winover = @play1.winner? || @play2.winner?
-    @over = true if winover || @board.full
-    @tie = true if @board.full && !winover
+    if @play1.winner? || @play2.winner?
+      @over = true
+      @tie = false
+    elsif @board.full
+      @over = true
+      @tie = true
+    end
     @p1turn = @p1turn ? false : true
   end
 end
