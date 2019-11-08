1851f9cda1a9f024775a041671af798987a587dc
kingsley McSimon Ogbonna <kingsley.mcsimon@gmail.com>
2019-11-07 21:36:11 -0800

    some linter complain corrected

--- spec/tic_tac_spec.rb
+++ spec/tic_tac_spec.rb
@@ -3,21 +3,21 @@ require_relative '../lib/board.rb'
 require_relative '../lib/game.rb'
 
 describe Player do
-  context "Player should be able to assign its name and return it" do
-    it "returns its name when .name is called" do
-      p1 = Player.new("Player1", "X")
+  context 'Player should be able to assign its name and return it' do
+    it 'returns its name when .name is called' do
+      p1 = Player.new('Player1', 'X')
       expect(p1.name).to be_instance_of(String)
     end
   end
-  context "Player should be able to know its symbol and return it" do
-    it "returns its name when .name is called" do
-      p1 = Player.new("Player1", "X")
-      expect(p1.symbol).to eq ("X")
+  context 'Player should be able to know its symbol and return it' do
+    it 'returns its name when .name is called' do
+      p1 = Player.new('Player1', 'X')
+      expect(p1.symbol).to eq ('X')
     end
   end
-  context "Player should be able to tell if it won or not" do
+  context 'Player should be able to tell if it won or not' do
     it "returns true if player has a winning combination in its 'taken' list" do
-      p1 = Player.new("Player1", "X")
+      p1 = Player.new('Player1', 'X')
       expect(p1.winner?).to eq false
       p1.taken = [1, 2, 3]
       expect(p1.winner?).to eq true
@@ -26,17 +26,17 @@ describe Player do
 end
 
 describe Board do
-  context "Board should be able to draw itself" do
-    it "returns an array" do
+  context 'Board should be able to draw itself' do
+    it 'returns an array' do
       board = Board.new
       expect(board.draw).to be_instance_of(Array)
     end
   end
   context "Board should be able to change its values with player's symbol" do
-    it "should change its inner variable to match the input" do
+    it 'should change its inner variable to match the input' do
       board = Board.new
-      p1 = Player.new("Player1", "X")
-      board.inner = ["X", "O", "X", "X", "O", "O", 7, "X", "O"] # to simulate last turn
+      p1 = Player.new('Player1', 'X')
+      board.inner = ['X', 'O', 'X', 'X', 'O', 'O', 7, 'X', 'O'] # to simulate last turn
       expect(board.full).to be false
       board.take_place(7, p1) # to select the last available option
       expect(board.full).to be true
@@ -45,11 +45,11 @@ describe Board do
 end
 
 describe Game do
-  context "Game should know whose turn it is" do
-    it "should toggle its p1turn value when its turn method is called" do
+  context 'Game should know whose turn it is' do
+    it 'should toggle its p1turn value when its turn method is called' do
       board = Board.new
-      p1 = Player.new("player1", "X")
-      p2 = Player.new("player2", "O")
+      p1 = Player.new('player1', 'X')
+      p2 = Player.new('player2', 'O')
       game = Game.new(p1, p2, board)
       expect(game.p1turn).to be true
       game.turn(1)
